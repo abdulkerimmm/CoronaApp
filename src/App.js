383ef5c17eb89095346dfb003d79036f69d86286
abdulkerim yalçın <64563946+abdulkerimmm@users.noreply.github.com>
2024-02-02 16:53:14 +0300

    deploy portfiloi

--- src/App.js
+++ src/App.js
@@ -8,10 +8,18 @@ import { context1 } from "./contextApi/Store";
 function App() {
   const [key] = useContext(context1);
 
+  const backgroundStyle = {
+    minHeight: "100vh", // Set minimum height to cover the entire viewport
+    backgroundColor: "#e5e5e5", // Replace with your desired background color
+    backgroundImage: 'url("your-background-image-url.jpg")', // If you have a background image
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+  };
+
   return (
-    <div className="min-h-screen bg-cover bg-center bg-gray-200">
+    <div style={backgroundStyle}>
       <Header />
-      <div className="md:container md:mx-auto p-8  page-content  ">
+      <div className="md:container md:mx-auto p-8  page-content ">
         {key === 1 && <CoronaForCountry></CoronaForCountry>}
         {key === 2 && <RecordedCorona></RecordedCorona>}
         {key === 3 && <CoronaForContinent></CoronaForContinent>}
